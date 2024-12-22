2185fae8344afa55529e1facd2f0ac6894b3e0a3
dpnetca <git@dpnet.ca>
2024-12-22 11:07:40 -0600

    rearrange thumb keys

--- keyboards/keebio/iris_ce/keymaps/dpnetca/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/dpnetca/keymap.c
@@ -19,15 +19,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                                         KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
             KC_EQL, KC_Q, HRM_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I, HRM_O, KC_P, KC_BSLS,
             CW_TOGG, HRM_A, HRM_S,HRM_D, HRM_F, KC_G,                                     KC_H, HRM_J, HRM_K, HRM_L, HRM_SCLN, KC_QUOT,
-            KC_LBRC, KC_Z, KC_X, KC_C, KC_V, KC_B,            KC_INS,           KC_APP,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
-                     LT(5,KC_ESC), LT(4,KC_BSPC), LT(1,KC_DEL),                   LT(3,KC_SPC), KC_ENT, LT(2,KC_TAB)
+            KC_LBRC, KC_Z, KC_X, KC_C, KC_V, KC_B,            KC_ESC,           KC_TAB,   KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
+                     LT(5,KC_INS), LT(4,KC_BSPC), LT(1,KC_DEL),                      KC_ENT, LT(3,KC_SPC), LT(2,KC_APP)
         ),
     [1] = LAYOUT(
             TG(6), KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME,                                   KC_LPRN, KC_NUM, KC_PSLS, KC_PAST, KC_PMNS, KC_PAUS,
-            KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_END,                                    KC_RPRN, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PSCR,
-            KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,                             KC_PERC, KC_P4, KC_P5, KC_P6, KC_PEQL, KC_SCRL,
-            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN,     KC_NO,             KC_NO,     KC_PCMM, KC_P1, KC_P2, KC_P3, KC_PENT, KC_NO,
-                                     KC_NO, KC_TRNS, KC_TRNS,                        KC_P0, KC_PDOT, KC_NO
+            KC_NO, KC_NO, KC_NO, KC_UP, KC_NO, KC_END,                                    KC_RPRN, KC_7, KC_8, KC_9, KC_PPLS, KC_PSCR,
+            KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,                             KC_PERC, KC_4, KC_5, KC_6, KC_PEQL, KC_SCRL,
+            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN,     KC_NO,             KC_NO,     KC_PCMM, KC_1, KC_2, KC_3, KC_PENT, KC_NO,
+                                     KC_NO, KC_TRNS, KC_TRNS,                        KC_PDOT, KC_P0, KC_NO
         ),
     [2] = LAYOUT(
             KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6,                                     KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
